e62a40ba15d2695cc8d5540fa4946b119f3e8aee
Jack-chau <chaukaho149@gmail.com>
2026-09-27 00:49:29 +0800

    try argparse

--- argumentParsing/adder.py
+++ argumentParsing/adder.py
@@ -1,15 +1,28 @@
+# This program calculates the volume of a cylinder given the radius and height
+# Cylinder volume formula: V = πr^2h
+import math
 import argparse
 
-parser = argparse.ArgumentParser( )
+parser = argparse.ArgumentParser( description = "Calculate the volume of a cylinder" )
+parser.add_argument( "-r", "--radius", metavar = '', required = True ,type = int, help = "The radius of the cylinder" )
+parser.add_argument( "-H", "--height", metavar = '', required = True ,type = int, help = "The height of the cylinder" )
+group = parser.add_mutually_exclusive_group()
+group.add_argument( '-q', '--quiet', action = 'store_true', help = "print quiet" ) 
+group.add_argument( '-v', '--verbose', action = 'store_true', help = "print verbose" )
 
-parser.add_argument( "greeting", help="The greeting message displayed" )
-parser.add_argument( "-n", "--numbers", type = float, nargs = 2, help="The numbers to add" )
 
-args = parser.parse_args( ) # returns args object
+args = parser.parse_args()
 
-print( args )
-print( args.greeting )
-print( args.numbers )
+def cylinder_volume(radius, height):
+    volume = math.pi * radius**2 * height
+    return round(volume, 2)
 
-if args.numbers is not None:
-    print( args.numbers[0] + args.numbers[1] )
+if __name__ == "__main__":
+    volume =  cylinder_volume( args.radius, args.height )
+    if args.quiet:
+        print( volume )
+    elif args.verbose:
+        print( f"The volume of the cylinder is {volume}" )
+    else:
+        print( f"The volume of the cylinder is {volume}" )
+    
